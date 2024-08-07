868de7bf63cdcc693743bbb3421af4e25376c8e9
hyparh <hyparh@gmail.com>
2024-08-07 23:09:33 +0300

    Configure routings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
-import { Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+import { HomeComponent } from "./home/home.component";
+import { MovieComponent } from "./movie/movie.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'movie', component: MovieComponent },
+    { path: '**', component: LoginComponent }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes),
+        LoginComponent,
+        HomeComponent,
+        MovieComponent
+    ],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
