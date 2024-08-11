be0840cfef805dbad7756b7bd4d104e93be26b51
hyparh <hyparh@gmail.com>
2024-08-11 17:31:59 +0300

    Validate credentials

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,11 +15,22 @@ export class LoginComponent {
   password = "";
   errorMsg = "";
 
+  constructor(private auth: AuthService, private router: Router) {}
+
   login() {
     if (this.username.trim().length === 0) {
       this.errorMsg = "Username is required";
     } else if (this.password.trim().length === 0) {
       this.errorMsg = "Password is required";
+    } else {
+      this.errorMsg = "";
+      let res = this.auth.login(this.username, this.password);
+      if (res === 200) {
+        this.router.navigate(['home']);
+      }
+      if (res === 403) {
+        this.errorMsg = 'Invalid Credentials';
+      }
     }
   }
 }
